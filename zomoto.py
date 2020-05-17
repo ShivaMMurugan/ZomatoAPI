3f957ccc335dace2c9659ce56fa38679792772bd
Siva <shivavijaya07@gmail.com>
2020-05-17 12:11:26 +0530

    Play with ZomatoDeveloper API

---
+++ zomoto.py
@@ -0,0 +1,80 @@
+# importing pyzomato module
+
+# requirement > pip install pyzomato
+
+
+# checkout geocode endpoint, very useful one to find restaurent id.
+
+# expore latitue and longitude for your city.
+
+import os
+from pyzomato import Pyzomato
+
+''' There is so much we can do with this API, there are 17 endpoints
+ we can configure and use however we want, I just used few to see what's
+ available in this api.'''
+
+class ZomotoAPI:
+
+
+	def __init__(self):
+
+		# Api secret keys stored in enviornment variable
+
+		self.api_key = os.environ.get('api_key')
+
+	def get_Categories(self, citi_id):
+
+		self.api_client_auth = Pyzomato(self.api_key)
+
+		self.api_client_auth.search(name="Chennai")
+
+		# city id for chennai is 7
+
+		return self.api_client_auth.getCategories()
+
+
+	def search_city_by_geo_code(self, latitude, longitude):
+
+		self. latitude = latitude
+		self.longitude = longitude
+
+		result = self.api_client_auth.getByGeocode(self.latitude, self.longitude)
+
+		return result
+
+
+	def locating_res_by_id(self, res_id):
+
+		self.res_id = res_id
+
+		# id > 18387708 
+		# name > kakada ramprasad
+		# "locality_verbose": "Kilpauk, Chennai"
+
+		result = self.api_client_auth.getRestaurantDetails(self.res_id)
+
+
+		return result
+
+
+
+def main():
+
+	zomoto = ZomotoAPI()
+
+	get_categories = zomoto.get_Categories(7)
+
+	search_city = zomoto.search_city_by_geo_code('13.067439', '80.237617')
+
+	locate = zomoto.locating_res_by_id('18387708')
+
+	print(get_categories)
+	print(search_city)
+	print(locate)
+
+if __name__ == '__main__':
+
+	main()
+
+
